3688c54530ddc18ad41e2428cd5952ac0525820e
steffiag <steffiageorge@gmail.com>
2025-06-08 17:33:13 -0400

    normal versionnnnn

---
+++ app.js
@@ -0,0 +1,17 @@
+// app.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello from Junior Midyear Project!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
